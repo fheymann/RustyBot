173eb4a074258132fe93e5114a364fa616fddf36
heym_fr <frank.heymann@dlr.de>
2026-02-17 17:34:51 +0000

    working astar algo

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
 use ndarray::prelude::*;
 use ndarray::{Array, Ix2};
+use pathfinding::prelude::astar;
 use rand::{Rng, SeedableRng, rngs::StdRng};
 use serde_json::Value;
+use std::collections::VecDeque;
 use std::io::{self, BufRead, Write};
 
 #[derive(serde::Serialize, serde::Deserialize)]
@@ -26,19 +28,54 @@ impl Highlight {
         self
     }
 
-    pub fn blue(mut self, pos_vec: Vec<Pos>, alpha: u8) -> Self {
+    pub fn blue(self, pos_vec: Vec<Pos>, alpha: u8) -> Self {
         self.color(pos_vec, "#0000ff", alpha)
     }
 
-    pub fn white(mut self, pos_vec: Vec<Pos>, alpha: u8) -> Self {
+    pub fn white(self, pos_vec: Vec<Pos>, alpha: u8) -> Self {
         self.color(pos_vec, "#ffffff", alpha)
     }
 }
 
-#[derive(serde::Serialize, serde::Deserialize)]
+#[derive(Copy, Clone, Debug, serde::Serialize, serde::Deserialize, PartialEq, Eq, Hash)]
 pub struct Pos {
     x: usize,
     y: usize,
+}
+
+impl Pos {
+    pub fn new(x: usize, y: usize) -> Self {
+        Self { x, y }
+    }
+
+    pub fn distance(&self, ref_pos: &Pos) -> u32 {
+        (self.x.abs_diff(ref_pos.x) + self.y.abs_diff(ref_pos.y)) as u32
+    }
+
+    pub fn a_star_succesors(&self, ref_pixel_map: &PixelMap) -> Vec<(Pos, u32)> {
+        let mut ret_vec = Vec::new();
+        if self.x > 0 {
+            if ref_pixel_map[(self.x - 1, self.y)] != PixelType::Wall.as_u8() {
+                ret_vec.push((Pos::new(self.x - 1, self.y), 1));
+            }
+        }
+        if self.x < ref_pixel_map.dim().0 - 1 {
+            if ref_pixel_map[(self.x + 1, self.y)] != PixelType::Wall.as_u8() {
+                ret_vec.push((Pos::new(self.x + 1, self.y), 1));
+            }
+        }
+        if self.y > 0 {
+            if ref_pixel_map[(self.x, self.y - 1)] != PixelType::Wall.as_u8() {
+                ret_vec.push((Pos::new(self.x, self.y - 1), 1));
+            }
+        }
+        if self.y < ref_pixel_map.dim().1 - 1 {
+            if ref_pixel_map[(self.x, self.y + 1)] != PixelType::Wall.as_u8() {
+                ret_vec.push((Pos::new(self.x, self.y + 1), 1));
+            }
+        }
+        ret_vec
+    }
 }
 
 impl TryFrom<&Value> for Pos {
@@ -101,13 +138,136 @@ pub fn map_to_pos_vec(ref_map: &PixelMap, pixel_type: PixelType) -> Vec<Pos> {
     ret_vec
 }
 
+#[derive(Debug)]
+pub struct GemList {
+    gem_vec: Vec<Gem>,
+}
+
+impl GemList {
+    pub fn new() -> Self {
+        Self {
+            gem_vec: Vec::new(),
+        }
+    }
+    pub fn exists(&self, ref_pos: &Pos) -> bool {
+        let mut exists = false;
+        for gem in &self.gem_vec {
+            if *gem.ref_pos() == *ref_pos {
+                exists = true;
+                break;
+            }
+        }
+        exists
+    }
+
+    pub fn add_gem(&mut self, pos: Pos, ttl: u64) {
+        if !self.exists(&pos) {
+            self.gem_vec.push(Gem::new(pos, ttl))
+        } else {
+            eprintln!("Gem exists at pos: {pos:?} with ttl: {ttl}");
+        }
+    }
+
+    pub fn next_tick(&mut self) {
+        for gem in &mut self.gem_vec {
+            gem.next_tick();
+        }
+    }
+
+    pub fn first(&self) -> Gem {
+        self.gem_vec[0]
+    }
+
+    pub fn len(&self) -> usize {
+        self.gem_vec.len()
+    }
+}
+
+#[derive(Debug, Copy, Clone)]
+pub struct Gem {
+    pos: Pos,
+    ttl: u64,
+}
+
+impl Gem {
+    pub fn new(pos: Pos, ttl: u64) -> Self {
+        Self { pos, ttl }
+    }
+
+    pub fn ref_pos(&self) -> &Pos {
+        &self.pos
+    }
+
+    pub fn ttl(&self) -> u64 {
+        self.ttl
+    }
+
+    pub fn next_tick(&mut self) {
+        self.ttl -= 1;
+    }
+}
+
 pub type PixelMap = Array<u8, Ix2>;
+
+pub struct Bot {
+    pos_hist: VecDeque<Pos>,
+    current_path: VecDeque<Pos>,
+}
+
+impl Bot {
+    pub fn new() -> Self {
+        Self {
+            pos_hist: VecDeque::new(),
+            current_path: VecDeque::new(),
+        }
+    }
+
+    pub fn update_pos(&mut self, pos: Pos) {
+        self.pos_hist.push_back(pos);
+    }
+
+    pub fn ref_current_pos(&self) -> Option<&Pos> {
+        self.pos_hist.back()
+    }
+
+    pub fn calculate_path(
+        &self,
+        ref_pixel_map: &PixelMap,
+        ref_gem_list: &GemList,
+    ) -> Option<(Vec<Pos>, u32)> {
+        if ref_gem_list.len() > 0 {
+            let ref_first_gem = ref_gem_list.first();
+            let ref_gem_pos = ref_first_gem.ref_pos();
+            let current_pos = self.ref_current_pos().unwrap();
+            let a_str_path = astar(
+                current_pos,
+                |p| p.a_star_succesors(ref_pixel_map),
+                |p| p.distance(ref_gem_pos),
+                |p| *p == *ref_gem_pos,
+            );
+            a_str_path
+        } else {
+            None
+        }
+    }
+
+    pub fn current_path(&self, ref_pixel_map: &PixelMap, ref_gem_list: &GemList) -> Vec<Pos> {
+        if let Some((path, v)) = self.calculate_path(ref_pixel_map, ref_gem_list) {
+            path
+        } else {
+            Vec::new()
+        }
+    }
+}
 
 fn main() {
     let mut rng = StdRng::seed_from_u64(1);
     let moves = ["N", "S", "E", "W", "WAIT"];
     let mut first_tick = true;
+    let mut gem_list = GemList::new();
+    let mut bot = Bot::new();
     let mut opt_map: Option<PixelMap> = None;
+
     // Read one JSON object per line from stdin
     for line in io::stdin().lock().lines() {
         if line.is_err() {
@@ -141,11 +301,13 @@ fn main() {
                 }
             }
         }
-
+        gem_list.next_tick();
         if let Some(Value::Object(map)) = data.as_ref() {
-            if let Some(Value::Array(bot_pos)) = map.get("bot") {
+            if let Some(bot_pos_json) = map.get("bot") {
                 //eprintln!("Map: {rust_map:?}");
-                eprintln!("Bot position: {bot_pos:?}");
+                //eprintln!("Bot position: {bot_pos:?}");
+                let bot_pos = bot_pos_json.try_into().unwrap();
+                bot.update_pos(bot_pos);
             }
             if let Some(Value::Array(wall_pixels)) = map.get("wall") {
                 for wall_pixel in wall_pixels {
@@ -153,6 +315,21 @@ fn main() {
                     if let Some(ref mut tmp_map) = opt_map {
                         //tmp_map[(wall_pixel, 0)] = 17;
                         tmp_map[(wall_pos.x, wall_pos.y)] = PixelType::Wall.into();
+                    }
+                }
+            }
+
+            if let Some(Value::Array(gem_vec_json)) = map.get("visible_gems") {
+                for gem_entry_json in gem_vec_json {
+                    //eprintln!("GemEntryJson: {gem_entry_json}");
+                    let ttl = if let Some(Value::Number(ttl_json)) = gem_entry_json.get("ttl") {
+                        ttl_json.as_u64().unwrap()
+                    } else {
+                        u64::default()
+                    };
+                    if let Some(gem_pos_json) = gem_entry_json.get("position") {
+                        let gem_pos: Pos = gem_pos_json.try_into().unwrap();
+                        gem_list.add_gem(gem_pos, ttl);
                     }
                 }
             }
@@ -164,9 +341,14 @@ fn main() {
         //let highlight_json = "{\"highlight\":[[2,2,\"#00ff0050\"]]}";
         //let pos_vec = vec![Pos { x: 2, y: 2 }];
         let wall_pos_vec = map_to_pos_vec(opt_map.as_ref().unwrap(), PixelType::Wall);
-        let highlight = Highlight::empty().blue(wall_pos_vec, 70);
+        let mut highlight = Highlight::empty().blue(wall_pos_vec, 70);
+        if let Some(ref_pixel_map) = opt_map.as_ref() {
+            let a_star_path = bot.current_path(ref_pixel_map, &gem_list);
+            highlight = highlight.white(a_star_path, 90);
+        }
         let highlight_json = serde_json::to_string(&highlight).unwrap();
-        //eprintln!("{highlight_json}");
+        eprintln!("current_bot_pos: {:?}", bot.ref_current_pos());
+        eprintln!("{gem_list:?}");
         println!("{} {highlight_json}", moves[move_index]);
         let _ = io::stdout().flush();
     }
